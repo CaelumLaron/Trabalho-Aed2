b6722f5acf57da8407315293c2f5e293cd3e741b
João Lucas Fernandes de Melo <45059616+LuskaSaur@users.noreply.github.com>
2019-04-03 12:38:52 -0400

    Create Trab3.c

---
+++ Trab3.c
@@ -0,0 +1,38 @@
+#include "stdio.h"
+#include "Vetor.h"
+#include <time.h>
+
+int main(){
+ 	int vetor1[100000];
+ 	int j =0,tam = 100000;
+ 	double tempo[3][30];
+ 	clock_t tempo1,tempo2;
+ 	double media[3];
+ 	char codes[3][30] = {"Bubble","Insection","Quick"};
+ 	for(int i=0;i<30;i++){
+ 		j = 0;
+ 		PreencheVetorDesordenado(tam,vetor1);
+ 		tempo1 = clock();
+ 		BubbleSort(tam,vetor1);
+ 		tempo2 = clock();
+ 		tempo[j][i] = ((double)tempo2-tempo1)/CLOCKS_PER_SEC;
+ 		j++;
+ 		tempo1 = clock();
+ 		InsectionSort(tam,vetor1);
+ 		tempo2 = clock();
+ 		tempo[j][i] = ((double)tempo2-tempo1)/CLOCKS_PER_SEC;
+ 		j++;
+ 		tempo1 = clock();
+ 		quickSort(tam,vetor1);
+ 		tempo2 = clock();
+ 		tempo[j][i] = ((double)tempo2-tempo1)/CLOCKS_PER_SEC;	
+ 	}
+ 	for(int i =0;i<3;i++){
+ 		printf("%s\n",codes[i]);
+ 		for(int j =0;j<30;j++){
+ 			printf("%lf\n", tempo[i][j]);
+ 			media[i]+=tempo[i][j];
+ 		}
+ 	}
+ 	printf("Media Bubble:%lf\n Media Insection:%lf\n MediaQuick:%lf\n",media[0]/30,media[1]/30,media[2]/30);
+ }
