e24999d4de9cb994e982a269da543ebfa95a7794
João Lucas Fernandes de Melo <45059616+LuskaSaur@users.noreply.github.com>
2019-04-03 13:13:24 -0400

    Update Trab3.c

--- Trab3.c
+++ Trab3.c
@@ -9,26 +9,28 @@ int main(){
  	clock_t tempo1,tempo2;
  	double media[3];
  	char codes[3][30] = {"Bubble","Insection","Quick"};
- 	for(int i=0;i<30;i++){
- 		j = 0;
+ 	for(int i=0,j=0;i<30;i++){
  		PreencheVetorDesordenado(tam,vetor1);
  		tempo1 = clock();
  		BubbleSort(tam,vetor1);
  		tempo2 = clock();
  		tempo[j][i] = ((double)tempo2-tempo1)/CLOCKS_PER_SEC;
- 		j++;
- 		tempo1 = clock();
- 		InsectionSort(tam,vetor1);
- 		tempo2 = clock();
- 		tempo[j][i] = ((double)tempo2-tempo1)/CLOCKS_PER_SEC;
- 		j++;
- 		tempo1 = clock();
- 		quickSort(tam,vetor1);
- 		tempo2 = clock();
- 		tempo[j][i] = ((double)tempo2-tempo1)/CLOCKS_PER_SEC;	
- 	}
+  }
+ 		for(int i=0,j=1;i<30;i++){
+    PreencheVetorDesordenado(tam,vetor1);
+ 		 tempo1 = clock();
+ 		 InsectionSort(tam,vetor1);
+ 		 tempo2 = clock();
+ 		 tempo[j][i] = ((double)tempo2-tempo1)/CLOCKS_PER_SEC;
+  }
+ 	 for(int i =0,j=0;i<30;i++){
+ 		 tempo1 = clock();
+ 		 quickSort(tam,vetor1);
+ 		 tempo2 = clock();
+ 		 tempo[j][i] = ((double)tempo2-tempo1)/CLOCKS_PER_SEC;	
+  }
  	for(int i =0;i<3;i++){
- 		printf("%s\n",codes[i]);
+ 		printf("Tempos do %s\n",codes[i]);
  		for(int j =0;j<30;j++){
  			printf("%lf\n", tempo[i][j]);
  			media[i]+=tempo[i][j];
