bb9f8007efa01cac93da5c6ed596e03cde511d32
João Lucas Fernandes de Melo <45059616+LuskaSaur@users.noreply.github.com>
2019-04-03 12:15:26 -0400

    Create Trab2.c

---
+++ Trab2.c
@@ -0,0 +1,50 @@
+#include "stdio.h"
+#include "Vetor.h"
+#include <time.h>
+
+int main(){
+	ListaEncadeada lista;
+ 	int vetor1[1000000],chave[30];
+ 	int tam = 1000000; 
+ 	clock_t tempo1,tempo2;
+ 	double media =0;
+ 	double tempo[30];
+ 	int vseq[30];
+ 	PreencheVetorOrdenado(tam, vetor1);
+ 	CriaListaEncadeada(&lista);
+ 	CriaListaEVetorIguais(tam,&lista, vetor1);
+	 for(int i =0;i<30;i++){
+	 	chave[i] =(rand()%100+1);
+	 }
+
+	for(int i =0;i<30;i++){
+		tempo1 = clock();
+		BuscaSequencialVetor(chave[i],tam,vetor1);
+		tempo2 = clock();
+		tempo[i] = ((double)(tempo2-tempo1)/(CLOCKS_PER_SEC/1000));
+	 }
+
+	 printf("BUSCA SEQUENCIAL VETOR \n");
+	 for(int i =0;i<30;i++){
+	 	printf("%lf\n",tempo[i]);
+	 }
+	 for(int i = 0;i<30;i++){
+	 	media +=tempo[i];
+	 }
+	 printf("\n%lf\n",(media/30));
+	 for(int i=0;i<30;i++){
+	 	tempo1 = clock();
+	 	BuscaSequencialListaEncadeada(chave[i], lista);
+	 	tempo2 = clock();
+	 	tempo[i] = ((double)(tempo2-tempo1)/(CLOCKS_PER_SEC/1000));
+	 }
+	 printf("BUSCA SEQUENCIAL LISTA \n");
+	 for(int i =0;i<30;i++){
+	 	printf("%lf\n",tempo[i]);
+	 }
+	 for(int i = 0;i<30;i++){
+	 	media +=tempo[i];
+	 }
+	 printf("\n%lf\n",(media/30));
+	 
+	}
