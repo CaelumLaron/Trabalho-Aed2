d6da6c57a8f9c4d18559204f144c49a5c36e8df6
João Lucas Fernandes de Melo <45059616+LuskaSaur@users.noreply.github.com>
2019-04-08 10:48:22 -0400

    Update Lista.h

--- Lista.h
+++ Lista.h
@@ -1,3 +1,5 @@
+#ifndef LISTA_H
+#define LISTA_H
 
 typedef struct Node{
 	int dado;
@@ -15,3 +17,5 @@ Node* CriaNode(int newDado); // cria um novo no
 void CriaNoListaEncadeada(ListaEncadeada *lista, int newDado); //Cria um novo no na Lista
 int BuscaSequencialListaEncadeada(int chave, ListaEncadeada lista); //Faz uma busca sequencial na lista
 void CriaListaEVetorIguais(int n, ListaEncadeada *lista, int v[]);//Cria lista e vetor de tamanho com mesmos elementos
+
+#endif
