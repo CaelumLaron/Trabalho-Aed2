326ab736d8e86c25c71dd8a98ffce35cc747f6f4
João Lucas Fernandes de Melo <45059616+LuskaSaur@users.noreply.github.com>
2019-04-08 10:49:40 -0400

    Update Vetor.c

--- Vetor.c
+++ Vetor.c
@@ -2,41 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-void CriaListaEncadeada(ListaEncadeada *lista){
-	lista->prim = NULL;
-}
-
-Node* CriaNode(int newDado){
-	Node *aux;
-	aux = (Node*)malloc(sizeof(Node));
-	aux->prox = NULL;
-	aux->dado = newDado;
-	return aux;
-}
-
-void CriaNoListaEncadeada(ListaEncadeada *lista, int newDado){
-	Node *i = lista->prim;
-	if(i){
-		Node *j = lista->last;
-		j->prox = CriaNode(newDado);
-		lista->last = j->prox;
-	}
-	else{
-		lista->prim = CriaNode(newDado);
-		lista->last = lista->prim;
-	}
-}
-
-int BuscaSequencialListaEncadeada(int chave, ListaEncadeada lista){
-	for(Node* i = lista.prim; ; i = i->prox){
-		if(!i)
-			break;
-		if(i->dado == chave)
-			return 1;
-	}
-	return 0;
-}
-
 void PreencheVetorOrdenado(int n, int v[]){
 	clock_t t;
 	srand((unsigned)time(t));
